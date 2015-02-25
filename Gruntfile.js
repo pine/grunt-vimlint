7339edd42cef55fe49f74a868253b3d92adb3548
Pine Mizune <pinemz@gmail.com>
2015-02-25 14:21:32 +0900

    Fix test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var exec = require('child_process').exec;
+
 module.exports = function(grunt) {
   grunt.initConfig({
     vimlint: {
@@ -10,6 +12,27 @@ module.exports = function(grunt) {
         src: ['test/failed.vim']
       }
     }
+  });
+  
+  grunt.registerTask('test', function() {
+    var self = this;
+    
+    var done = self.async();
+    exec('grunt vimlint:succeeded', function (err, stdout, stderr) {
+      if (err) {
+        grunt.log.error(stdout || stderr);
+        return done(err);
+      }
+      
+      exec('grunt vimlint:failed', function (err, stdout, stderr) {
+        if (!err) {
+          grunt.log.error('vimlint succeeded');
+          return done(true);
+        }
+        
+        done();
+      });
+    });
   });
   
   grunt.loadTasks('tasks');
