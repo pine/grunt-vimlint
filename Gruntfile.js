e355c8ccb80a87fda1aa4d23987f58e6f8cc2bff
Pine Mizune <pinemz@gmail.com>
2015-02-27 07:43:59 +0900

    Add jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,17 @@
 'use strict';
 
 module.exports = function(grunt) {
+  grunt.initConfig({
+    jshint: {
+      options: {
+        jshintrc: true
+      },
+      files: ['*.js', 'tasks/**/*.js', 'test/**/*.js']
+    }
+  });
+  
   grunt.loadTasks('tasks');
   grunt.loadTasks('test/tasks');
+  
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 };
